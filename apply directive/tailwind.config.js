60a1c5e2eddceb4035608ecd859b914ae2cacfc1
Jeevan <gairejeevan7@gmail.com>
2024-07-20 11:31:48 +0530

    customizing tailwind css

--- apply directive/tailwind.config.js
+++ apply directive/tailwind.config.js
@@ -1,9 +1,26 @@
+// npx tailwindcss init confJeevan init --full
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   
   content: ["*"],
   theme: {
-    extend: {},
+    screens: {
+      xsm: '340px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      spacing:{
+        13: '3.5rem'
+      },
+      fontSize: {
+        '10xl' : ['9rem', { lineHeight: '1.2'}]
+      },
+    },
   },
   
   plugins: [],
